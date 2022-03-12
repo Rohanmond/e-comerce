cf53869533a3863496d275ad6de4fc0bf718b024
Rohan Mondal <mondalrohan108@gmail.com>
2022-03-12 14:18:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import './App.css';
+import "./App.css";
 import Address from "./Address/Address";
-import NavBar from "./NavBar/NabBar"
-import Wishlist from "./Wishlist/Wishlist"
-import Product from "./ProductListing/Product"
-import {usePage} from "./PageContext"
-import {IconProvider} from "./WishIcon"
-import {WishCounterProvider} from "./WishlistCounter"
-import {CartCounterProvider} from "./CartItemCounter"
-import Cart  from "./Cart/Cart"
+import NavBar from "./NavBar/NabBar";
+import Wishlist from "./Wishlist/Wishlist";
+import Product from "./ProductListing/Product";
+import { usePage } from "./PageContext";
+import { IconProvider } from "./WishIcon";
+import { WishCounterProvider } from "./WishlistCounter";
+import { CartCounterProvider } from "./CartItemCounter";
+import Cart from "./Cart/Cart";
 function App() {
-  const {page, clickHandler} = usePage()
+  const { page, clickHandler } = usePage();
   return (
     <div className="App">
-      <button onClick = {clickHandler}>Products</button>
-      <button onClick = {clickHandler}>Wishlist</button>
-      <button onClick = {clickHandler}>Cart</button>
-       <WishCounterProvider> 
+      <button onClick={clickHandler}>Products</button>
+      <button onClick={clickHandler}>Wishlist</button>
+      <button onClick={clickHandler}>Cart</button>
+      <WishCounterProvider>
         <IconProvider>
-        <CartCounterProvider>
+          <CartCounterProvider>
+            <div>
+              <NavBar />
 
-          <div>
-               <NavBar />
-              
-               {page === "Products" &&<Product />}
-            {page === "Wishlist" &&<Wishlist />}
-            {page === "Cart" &&<Cart />}
-            {/* <Cart /> */}
-
-          </div>
+              {page === "Products" && <Product />}
+              {page === "Wishlist" && <Wishlist />}
+              {page === "Cart" && <Cart />}
+              {/* <Cart /> */}
+            </div>
           </CartCounterProvider>
-
         </IconProvider>
-        </WishCounterProvider>
-        
-        
+      </WishCounterProvider>
+
       {/* <Address /> */}
     </div>
-    
   );
 }
 
